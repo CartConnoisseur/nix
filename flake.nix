3e745df545120845858d78402a76dcf2e0a1535a
Caroline Larimore <caroline@larimo.re>
2024-05-11 19:12:09 -0700

    Remove old unused code

--- flake.nix
+++ flake.nix
@@ -4,11 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    #disko = {
-    #  url = "github:nix-community/disko";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
-
     impermanence = {
       url = "github:nix-community/impermanence";
     };
@@ -24,9 +19,6 @@
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
-        #inputs.disko.nixosModules.default
-        #(import ./disko.nix { device = "/dev/vda"; })
-
         ./configuration.nix
               
         inputs.home-manager.nixosModules.default
