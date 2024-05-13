c7bdfbb27406d90f50e5d46908e0a4e1f96096b2
Caroline Larimore <caroline@larimo.re>
2024-05-12 20:06:48 -0700

    "Reformat" flameshot config

--- modules/home/flameshot.nix
+++ modules/home/flameshot.nix
@@ -7,15 +7,14 @@
     settings = let c = config.theme.colors; in {
       General = {
         savePath = "Pictures/Screenshots";
+        filenamePattern = "%F_%T";
+
+        saveAfterCopy = true;
 
         uiColor = "#${c.bg}";
         contrastUiColor = "#${c.accent}";
 
-
-        filenamePattern = "%F_%T";
-
         startupLaunch = false;
-        saveAfterCopy = true;
       };
     };
   };
