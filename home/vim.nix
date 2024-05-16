e8499fa329adea159e0ea8d28570b04db367c16f
Caroline Larimore <caroline@larimo.re>
2024-05-15 21:24:56 -0700

    Configure vim

--- home/vim.nix
+++ home/vim.nix
@@ -1,7 +1,35 @@
-{ ... }:
+{ pkgs, ... }:
+
+# Honestly, I'm pretty disappointed with
+#   home-manager's `programs.vim`. It's kinda bad and
+#   won't let me run `highlight` after `colorscheme`
 
 {
-  programs.vim = {
-    enable = true;
+  home.file.".vim/vimrc".text = ''
+    set number
+    set relativenumber
+
+    set termguicolors
+    set background=dark
+    colorscheme gruvbox
+
+    syntax on
+
+    " Awesome magical color override from
+    " https://gist.github.com/romainl/379904f91fa40533175dfaec4c833f2f
+    augroup MyColors
+      autocmd!
+      autocmd ColorScheme * highlight Normal guibg=#000000
+    augroup END
+  '';
+
+  home.file.".vim/pack/default/start/gruvbox".source = builtins.fetchGit {
+    url = "https://github.com/morhetz/gruvbox.git";
+    rev = "f1ecde848f0cdba877acb0c740320568252cc482";
+  };
+
+  home.file.".vim/pack/all/start/vim-nix".source = builtins.fetchGit {
+    url = "https://github.com/LnL7/vim-nix.git";
+    rev = "e25cd0f2e5922f1f4d3cd969f92e35a9a327ffb0";
   };
 }
