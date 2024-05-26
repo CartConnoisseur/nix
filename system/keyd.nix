5aa72e08d2c69eedf816f532b8e634c269f3d270
Caroline Larimore <caroline@larimo.re>
2024-05-25 20:46:57 -0700

    Edit comment

--- system/keyd.nix
+++ system/keyd.nix
@@ -7,7 +7,7 @@
     keyboards."*".settings = {
       main = {
         # Swap alt and meta keys.
-        #   I prefer (physical) alt as my WM modifier key because it
+        # I prefer (physical) alt as my WM modifier key because it
         #   is easier to reach. This can collide with some programs
         #   shortcuts if they inlcude alt. Swapping alt and meta fixes
         #   this by making my WM mod key (software) meta, freeing up alt.
