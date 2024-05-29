33f70ffdb63403bb6fb8f421a29bb17164d8b59c
Caroline Larimore <caroline@larimo.re>
2024-05-28 17:34:36 -0700

    Add missing theme colors to kitty

--- home/kitty.nix
+++ home/kitty.nix
@@ -27,6 +27,37 @@
       color13 = "#${c.brightMagenta}";
       color14 = "#${c.brightCyan}";
       color15 = "#${c.brightWhite}";
+
+
+      color124 = "#${c.brightRed}";
+      color106 = "#${c.brightGreen}";
+      color172 = "#${c.brightYellow}";
+      color66  = "#${c.brightBlue}";
+      color132 = "#${c.brightMagenta}";
+      color72  = "#${c.brightCyan}";
+
+      color167 = "#${c.red}";
+      color142 = "#${c.green}";
+      color214 = "#${c.yellow}";
+      color109 = "#${c.blue}";
+      color175 = "#${c.magenta}";
+      color108 = "#${c.cyan}";
+
+      color234 = "#${c.bg0}";
+      color237 = "#${c.bg1}";
+      color239 = "#${c.bg2}";
+      color241 = "#${c.bg3}";
+      color243 = "#${c.bg4}";
+      color245 = "#${c.white}";
+
+      color246 = "#${c.fg4}";
+      color248 = "#${c.fg3}";
+      color250 = "#${c.fg2}";
+      color223 = "#${c.fg1}";
+      color229 = "#${c.fg0}";
+
+      color166 = "#${c.orange}";
+      color208 = "#${c.brightOrange}";
     };
 
     shellIntegration = {
