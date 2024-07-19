132ffeae5b7d4415a34d6c57748c193febe1bac9
Caroline Larimore <caroline@larimo.re>
2024-07-19 00:03:41 -0700

    Remove ununsed hardware config

--- hosts/copenhagen/hardware-configuration.nix
+++ hosts/copenhagen/hardware-configuration.nix
@@ -1,46 +1,11 @@
 { config, lib, pkgs, modulesPath, ... }:
 
+#TODO: regenerate on physical copenhagen hardware
+
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  # boot = {
-  #   loader.grub = {
-  #     enable = true;
-
-  #     efiSupport = true;
-  #     efiInstallAsRemovable = true;
-  #     mirroredBoots = [
-  #       { devices = [ "nodev" ]; path = "/boot"; }
-  #     ];
-  #   };
-
-  #   initrd = {
-  #     availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-  #     kernelModules = [ ];
-  #   };
-
-  #   kernelModules = [ "kvm-amd" ];
-  #   extraModulePackages = [ ];
-
-  #   supportedFilesystems = [ "ntfs" ];
-  # };
-
-  # fileSystems = {
-  #   "/"        = { fsType = "zfs"; device = "zpool/root"; };
-  #   "/home"    = { fsType = "zfs"; device = "zpool/home"; };
-  #   "/persist" = { fsType = "zfs"; device = "zpool/persist"; };
-  #   "/nix"     = { fsType = "zfs"; device = "zpool/nix"; };
-
-  #   "/boot"    = { fsType = "vfat"; device = "/dev/disk/by-uuid/12CE-A600"; };
-
-  #   "/mnt/4tb" = { fsType = "ext4"; device = "/dev/disk/by-label/4tb"; };
-  #   "/mnt/ssd" = { fsType = "ext4"; device = "/dev/disk/by-label/ssd-256"; };
-  # };
-
-  # swapDevices = [ ];
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  # hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
