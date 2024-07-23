f36c64f008c8d822e42f325bfb9508844db552f4
Caroline Larimore <caroline@larimo.re>
2024-07-22 19:59:12 -0700

    Setup git on copenhagen

--- hosts/copenhagen/home/c.nix
+++ hosts/copenhagen/home/c.nix
@@ -17,6 +17,26 @@
     allowOther = true;
   };
 
+
+  programs = {
+    git = {
+      enable = true;
+
+      userName = "Caroline Larimore";
+      userEmail = "caroline@larimo.re";
+
+      signing = {
+        key = "DE64538967CA0C68";
+        signByDefault = true;
+      };
+
+      ignores = [
+        "*~"
+        "*.swp"
+      ];
+    };
+  };
+
   home.packages = with pkgs; [
     cloc
   ];
