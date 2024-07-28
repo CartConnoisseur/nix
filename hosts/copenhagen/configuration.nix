936fdacc4845db393a03aa1479fc269e8718bd48
Caroline Larimore <caroline@larimo.re>
2024-07-28 09:30:46 -0700

    copenhagen: add phoenix ssh key

--- hosts/copenhagen/configuration.nix
+++ hosts/copenhagen/configuration.nix
@@ -45,7 +45,10 @@
       isNormalUser = true;
       hashedPasswordFile = "/secrets/passwords/c";
       extraGroups = [ "wheel" "minecraft" ];
-      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDO8JxqS7B2n3YlNtlVMZGARi+GG/z7wLiiyl52qSZc caroline@larimo.re" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDO8JxqS7B2n3YlNtlVMZGARi+GG/z7wLiiyl52qSZc caroline@larimo.re" # c-pc
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGftQ5W8QMIVhgDijreliiMgIqwQvxwTkpMftJdQWu+ caroline@larimo.re" # phoenix
+      ];
     };
   };
 
