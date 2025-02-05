b687747dd705a9f37c87f322f3628a32191ab246
Caroline Larimore <caroline@larimo.re>
2025-02-04 22:28:14 -0800

    migration: remove old flake configs

--- flake.nix
+++ flake.nix
@@ -18,46 +18,24 @@
     nix-minecraft.url = "github:CartConnoisseur/nix-minecraft";
   };
 
-  outputs = inputs:
-    inputs.snowfall-lib.mkFlake {
-      inherit inputs;
-      src = ./.;
+  outputs = inputs: inputs.snowfall-lib.mkFlake {
+    inherit inputs;
+    src = ./.;
 
-      snowfall = {
-        root = ./snowfall;
-        namespace = "cxl";
+    snowfall = {
+      root = ./snowfall;
+      namespace = "cxl";
 
-        meta = {
-          name = "cxl";
-          title = "cxl flake";
-        };
+      meta = {
+        name = "cxl";
+        title = "cxl flake";
       };
-
-      systems.modules.nixos = with inputs; [
-        inputs.home-manager.nixosModules.default
-        impermanence.nixosModules.impermanence
-        nix-minecraft.nixosModules.minecraft-servers
-      ];
     };
-  # nixosConfigurations = {
-  #   c-pc = nixpkgs.lib.nixosSystem {
-  #     specialArgs = { inherit inputs; };
-  #     modules = [
-  #       ./hosts/c-pc/configuration.nix
-  #       inputs.home-manager.nixosModules.default
-  #       inputs.impermanence.nixosModules.impermanence
-  #       inputs.nix-minecraft.nixosModules.minecraft-servers
-  #       (import ./overlays)
-  #     ];
-  #   };
 
-  #   copenhagen = nixpkgs.lib.nixosSystem {
-  #     specialArgs = { inherit inputs; };
-  #     modules = [
-  #       ./hosts/copenhagen/configuration.nix
-  #       inputs.home-manager.nixosModules.default
-  #       inputs.impermanence.nixosModules.impermanence
-  #       inputs.nix-minecraft.nixosModules.minecraft-servers
-  #     ];
-  #   };
+    systems.modules.nixos = with inputs; [
+      inputs.home-manager.nixosModules.default
+      impermanence.nixosModules.impermanence
+      nix-minecraft.nixosModules.minecraft-servers
+    ];
+  };
 }
