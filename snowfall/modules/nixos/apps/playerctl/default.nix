10f11ec042ab468821274636d438f84781cb2408
Caroline Larimore <caroline@larimo.re>
2025-02-04 18:50:11 -0800

    migration: desktop role

---
+++ snowfall/modules/nixos/apps/playerctl/default.nix
@@ -0,0 +1,18 @@
+{ options, config, lib, pkgs, namespace, ... }:
+
+with lib; with lib.${namespace}; let
+  cfg = config.${namespace}.apps.playerctl;
+in {
+  options.${namespace}.apps.playerctl = with types; {
+    enable = mkEnableOption "playerctl";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      playerctl
+    ];
+
+    #TODO: enable, eventually
+    services.playerctld.enable = false;
+  };
+}
---
+++ snowfall/modules/nixos/hardware/keyboard/default.nix
@@ -0,0 +1,43 @@
+{ options, config, lib, pkgs, namespace, ... }:
+
+with lib; with lib.${namespace}; let
+  cfg = config.${namespace}.hardware.keyboard;
+in {
+  options.${namespace}.hardware.keyboard = with types; {
+    enable = mkEnableOption "keyboard hardware tweaks";
+    jp = mkEnableOption "japanese ime support";
+  };
+
+  config = mkIf cfg.enable {
+    services.keyd = {
+      enable = true;
+      
+      keyboards."*".settings = {
+        main = {
+          # Swap alt and meta keys.
+          # I prefer (physical) alt as my WM modifier key because it
+          #   is easier to reach. This can collide with some programs
+          #   shortcuts if they inlcude alt. Swapping alt and meta fixes
+          #   this by making my WM mod key (software) meta, freeing up alt.
+
+          leftalt = "leftmeta";
+          leftmeta = "leftalt";
+
+          rightalt = "rightmeta";
+          rightmeta = "rightalt";
+        };
+      };
+    };
+
+    i18n.inputMethod = mkIf cfg.jp {
+      enable = true;
+      type = "fcitx5";
+      fcitx5.addons = with pkgs; [ fcitx5-mozc ];
+    };
+
+    environment.variables = mkIf cfg.jp {
+      # Required for fcitx5 support in kitty
+      GLFW_IM_MODULE = "ibus";
+    };
+  };
+}
