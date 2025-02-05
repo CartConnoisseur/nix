6cb87cfe56549fafe0d0a100b7c484d25337b46b
Caroline Larimore <caroline@larimo.re>
2025-02-05 15:49:45 -0800

    migration: jq

--- core/packages.nix
+++ core/packages.nix
@@ -4,7 +4,5 @@
   environment.systemPackages = with pkgs; [
     (writeShellScriptBin "rb" "sudo nixos-rebuild switch --flake /etc/nixos")
     (writeShellScriptBin "rbf" "sudo nixos-rebuild switch --flake path:/etc/nixos")
-    
-    jq
   ];
 }
