f0f6502ca09ec35b8fc5122948888e744fd41165
Caroline Larimore <caroline@larimo.re>
2025-02-07 21:11:17 -0800

    migration: home impermanence

---
+++ snowfall/modules/home/impermanence/default.nix
@@ -0,0 +1,50 @@
+{ options, config, lib, namespace, inputs, host, ... }:
+
+with lib; with lib.${namespace}; let
+  cfg = config.${namespace}.impermanence;
+  hosts = [ "c-pc" ];
+in {
+  imports = [
+    inputs.impermanence.nixosModules.home-manager.impermanence
+  ];
+  
+  options.${namespace}.impermanence = with types; {
+    skeleton = mkOption {
+      type = bool;
+      default = true;
+    };
+
+    location = mkOption {
+      type = str;
+      default = "/persist/home";
+    };
+
+    secure = {
+      location = mkOption {
+        type = str;
+        default = "/persist/secure/home";
+      };
+    };
+  };
+
+  config = {
+    home.persistence.${cfg.location} = {
+      enable = builtins.elem host hosts;
+      allowOther = true;
+
+      directories = mkIf cfg.skeleton [
+        "Downloads"
+        "Documents"
+        "Pictures"
+        "Videos"
+        "Music"
+        "Games"
+        "Persist"
+      ];
+    };
+
+    home.persistence.${cfg.secure.location} = {
+      enable = builtins.elem host hosts;
+    };
+  };
+}
