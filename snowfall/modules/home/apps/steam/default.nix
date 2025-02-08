166d2a7de6039a51283a92cf28065af787b2fb97
Caroline Larimore <caroline@larimo.re>
2025-02-08 15:09:38 -0800

    migration: persist steam files

---
+++ snowfall/modules/home/apps/steam/default.nix
@@ -0,0 +1,30 @@
+{ options, config, osConfig, lib, pkgs, namespace, ... }:
+
+#NOTE: steam must be installed system-wide.
+# This module does nothing unless steam is enabled at the
+# system level as well. This is only a separate toggle so
+# that not every user gets steam files in their home.
+with lib; with lib.${namespace}; let
+  cfg = config.${namespace}.apps.steam;
+  oscfg = osConfig.${namespace}.apps.steam;
+  impermanence = config.${namespace}.impermanence;
+in {
+  options.${namespace}.apps.steam = with types; {
+    enable = mkEnableOption "steam";
+  };
+
+  config = mkIf (cfg.enable && oscfg.enable) {
+    home.persistence.${impermanence.location} = {
+      directories = [{
+        directory = ".local/share/Steam";
+        method = "symlink";
+      }];
+    };
+
+    home.file = {
+      "Links/Steam Games".source = config.lib.file.mkOutOfStoreSymlink (
+        "${config.home.homeDirectory}/.local/share/Steam/steamapps/common"
+      );
+    };
+  };
+}
