2f17c2845eb2732e4c52a45c1d5dc186544aeef1
Caroline Larimore <caroline@larimo.re>
2025-02-08 17:02:27 -0800

    migration: nicotine

--- snowfall/modules/home/apps/default.nix
+++ snowfall/modules/home/apps/default.nix
@@ -1,13 +1,20 @@
-{ lib, namespace, ... }:
+{ lib, pkgs, namespace, ... }:
 
 with lib; with lib.${namespace}; {
-  imports = [
+  imports = with pkgs; [
     (mkSimpleApp "gimp" {})
     (mkSimpleApp "qbittorrent" {
       persist = [
         ".config/qBittorrent"
         ".local/share/qBittorrent"
         ".cache/qBittorrent"
+      ];
+    })
+    (mkSimpleApp "nicotine" {
+      packages = [ nicotine-plus ];
+      persist = [
+        ".config/nicotine"
+        ".local/share/nicotine"
       ];
     })
   ];
