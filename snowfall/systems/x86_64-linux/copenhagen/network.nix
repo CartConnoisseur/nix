8d2172fc5b62024414526b28d614ed85842efef7
Caroline Larimore <caroline@larimo.re>
2025-04-14 17:38:18 -0700

    migration: disable copenhagen wifi

--- snowfall/systems/x86_64-linux/copenhagen/network.nix
+++ snowfall/systems/x86_64-linux/copenhagen/network.nix
@@ -3,18 +3,6 @@
 {
   networking = {
     useDHCP = true;
-
-    wireless = {
-      enable = true;
-      
-      # Import /etc/wpa_supplicant.conf networks
-      allowAuxiliaryImperativeNetworks = true;
-    };
-
     firewall.enable = true;
-  };
-
-  environment.etc."wpa_supplicant.conf" = {
-    source = "/secrets/wireless.conf";
   };
 }
