b99d15f29e4e6079a14b423efdfbd49b00eeb62b
Caroline Larimore <caroline@larimo.re>
2025-04-16 09:06:38 -0700

    copenhagen: update ssh keys

--- systems/x86_64-linux/copenhagen/default.nix
+++ systems/x86_64-linux/copenhagen/default.nix
@@ -74,7 +74,7 @@ with lib; with lib.${namespace}; {
 
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDO8JxqS7B2n3YlNtlVMZGARi+GG/z7wLiiyl52qSZc caroline@larimo.re" # c-pc
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGftQ5W8QMIVhgDijreliiMgIqwQvxwTkpMftJdQWu+ caroline@larimo.re" # phoenix
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqMMrD6jbxzI07ox5NpJHHfqshsvz5KHR6GqVbpYIxr c@phone"
       ];
 
       extraGroups = [ "minecraft" ];
