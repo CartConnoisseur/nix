1ef2c356993bc14e850698ad6bb3128d111f8259
Caroline Larimore <caroline@larimo.re>
2025-04-16 08:39:55 -0700

    flake: update

--- modules/home/apps/vscode/default.nix
+++ modules/home/apps/vscode/default.nix
@@ -12,52 +12,54 @@ in {
       enable = true;
       package = pkgs.vscodium;
 
-      extensions = with pkgs.vscode-extensions; [
-        jdinhlife.gruvbox
-        vscode-icons-team.vscode-icons
+      profiles.default = {
+        extensions = with pkgs.vscode-extensions; [
+          jdinhlife.gruvbox
+          vscode-icons-team.vscode-icons
 
-        jnoortheen.nix-ide
-        golang.go
-        ziglang.vscode-zig
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "shader";
-          publisher = "slevesque";
-          version = "1.1.5";
-          sha256 = "sha256-Pf37FeQMNlv74f7LMz9+CKscF6UjTZ7ZpcaZFKtX2ZM=";
-        }
-        {
-          name = "yuck";
-          publisher = "eww-yuck";
-          version = "0.0.3";
-          sha256 = "sha256-DITgLedaO0Ifrttu+ZXkiaVA7Ua5RXc4jXQHPYLqrcM=";
-        }
-      ];
+          jnoortheen.nix-ide
+          golang.go
+          ziglang.vscode-zig
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "shader";
+            publisher = "slevesque";
+            version = "1.1.5";
+            sha256 = "sha256-Pf37FeQMNlv74f7LMz9+CKscF6UjTZ7ZpcaZFKtX2ZM=";
+          }
+          {
+            name = "yuck";
+            publisher = "eww-yuck";
+            version = "0.0.3";
+            sha256 = "sha256-DITgLedaO0Ifrttu+ZXkiaVA7Ua5RXc4jXQHPYLqrcM=";
+          }
+        ];
 
-      userSettings = {
-        "workbench.colorTheme" = "Gruvbox Dark Medium";
-        "workbench.iconTheme" = "vscode-icons";
-        "window.titleBarStyle" = "custom";
-        "editor.fontFamily" = "monospace";
+        userSettings = {
+          "workbench.colorTheme" = "Gruvbox Dark Medium";
+          "workbench.iconTheme" = "vscode-icons";
+          "window.titleBarStyle" = "custom";
+          "editor.fontFamily" = "monospace";
 
-        "git.confirmSync" = false;
+          "git.confirmSync" = false;
 
-        "vsicons.dontShowNewVersionMessage" = true;
+          "vsicons.dontShowNewVersionMessage" = true;
 
-        "files.associations" = {
-          "*.vsh" = "glsl";
-          "*.fsh" = "glsl";
-          "*.gsh" = "glsl";
+          "files.associations" = {
+            "*.vsh" = "glsl";
+            "*.fsh" = "glsl";
+            "*.gsh" = "glsl";
+          };
+
+          # Zig
+          "zig.initialSetupDone" = true;
+          "zig.path" = "";
+          "zig.formattingProvider" = "off";
+
+          "zig.zls.path" = "";
+          "zig.zls.enableAutofix" = false;
+          "zig.zls.enableInlayHints" = false;
         };
-
-        # Zig
-        "zig.initialSetupDone" = true;
-        "zig.path" = "";
-        "zig.formattingProvider" = "off";
-
-        "zig.zls.path" = "";
-        "zig.zls.enableAutofix" = false;
-        "zig.zls.enableInlayHints" = false;
       };
     };
 
