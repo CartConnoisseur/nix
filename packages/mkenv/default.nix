d6409d34c41191491c4ec4bf9022db6264c10ea4
Caroline Larimore <caroline@larimo.re>
2025-05-30 19:43:52 -0700

    mkenv: replace default flake

--- packages/mkenv/default.nix
+++ packages/mkenv/default.nix
@@ -9,7 +9,25 @@ pkgs.writeShellScriptBin "mkenv" ''
   if [[ -f flake.nix ]]; then
     echo "flake exists, skipping"
   else
-    nix flake init
+    cat <<EOF > flake.nix
+{
+  description = "very cool flake :3";
+  
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+
+      ];
+    };
+  };
+}
+EOF
   fi
 
   if [[ -f .envrc ]]; then
