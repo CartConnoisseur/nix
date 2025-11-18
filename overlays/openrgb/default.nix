e634c4748fa47285fd50d60bea3a03cdc20d92c4
Caroline Larimore <caroline@larimo.re>
2025-11-18 14:42:47 -0800

    openrgb: downgrade and patch

--- overlays/openrgb/default.nix
+++ overlays/openrgb/default.nix
@@ -2,19 +2,8 @@
 
 final: prev: {
   openrgb = prev.openrgb.overrideAttrs (old: {
-    src = prev.fetchFromGitLab {
-      owner = "CalcProgrammer1";
-      repo = "OpenRGB";
-      rev = "44c839c1160864c25170306bc1ab392333a682af";
-      hash = "sha256-k/Rt8VgaZJGktKj/Lv9G/EwtFykk1n1K2Mc3unEpF48=";
-    };
-
-    postPatch = ''
-      patchShebangs scripts/build-udev-rules.sh
-      substituteInPlace scripts/build-udev-rules.sh \
-        --replace /bin/chmod "${prev.coreutils}/bin/chmod"
-      substituteInPlace scripts/build-udev-rules.sh \
-        --replace /usr/bin/env "${prev.coreutils}/bin/env"
-    '';
+    patches = [
+      ./g733.patch
+    ] ++ prev.openrgb.patches;
   });
 }
